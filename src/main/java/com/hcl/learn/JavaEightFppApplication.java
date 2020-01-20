04e8f818b4159681760130392bb82c18b115428b
manish436 <manish436@gmail.com>
2020-01-20 14:29:40 +0530

    Initial commit

---
+++ src/main/java/com/hcl/learn/JavaEightFppApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEightFppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEightFppApplication.class, args);
+	}
+
+}
